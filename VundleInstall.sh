eb00c41d427b2545494e4f0b99519425e22477dc
Sahil Upadhye <40065580+SahilUpadhye291998@users.noreply.github.com>
2020-07-13 09:32:30 +0530

    Update Vim Plug

--- VundleInstall.sh
+++ VundleInstall.sh
@@ -1,3 +1,6 @@
 sudo apt install git
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt install curl
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
